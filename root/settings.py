2709063ed2322a8bb7a1b90d9e32a599cae5d5b2
Jaloliddin <kjaloliddin21@gmail.com>
2025-01-13 08:30:34 +0500

    add

--- root/settings.py
+++ root/settings.py
@@ -75,12 +75,15 @@ WSGI_APPLICATION = 'root.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "homework2",
+        "USER": "postgres",
+        "PASSWORD": "0951",
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
